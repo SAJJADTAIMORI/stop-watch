e1b0a88b1764bd6a258de1ce029f4ea096b55e21
SAJJAD TAIMORI <153691357+SAJJADTAIMORI@users.noreply.github.com>
2024-05-01 00:09:50 +0500

    js function end

--- app.js
+++ app.js
@@ -14,3 +14,24 @@ function startStop() {
     running = true;
   }
 }
+function updateDisplay() {
+    let currentTime = new Date().getTime();
+    let elapsedTime = currentTime - startTime;
+    let minutes = Math.floor(elapsedTime / (1000 * 60));
+    let seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+  
+    document.querySelector('.display').innerText = formatTime(minutes) + ':' + formatTime(seconds) + ':' + formatTime(milliseconds);
+  }
+  
+  function formatTime(time) {
+    return time < 10 ? '0' + time : time;
+  }
+  
+  function reset() {
+    clearInterval(timer);
+    document.querySelector('.display').innerText = '00:00:00';
+    document.getElementById('startStop').innerText = 'Start';
+    running = false;
+  }
+  
